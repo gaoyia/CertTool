f81050b3f38e9a004b4e1511c6a68a96451e6e2e
gaoyi <gaoyia@qq.com>
2025-07-16 17:22:16 +0800

    添加element-plus组件库

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,7 +1,11 @@
 import 'normalize.css'
 import './assets/main.css'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
+
+app.use(ElementPlus)
 app.mount('#app')
