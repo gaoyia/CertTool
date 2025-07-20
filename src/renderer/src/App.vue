6169aa18c5ad274203183cb6bb52ef0446d09b9f
gaoyi <gaoyia@qq.com>
2025-07-20 12:39:45 +0800

    优化赛博要饭按钮

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-// 不需要额外的脚本逻辑
+import { Star } from '@element-plus/icons-vue'
 </script>
 
 <template>
   <div class="app-container">
+    <div class="donate-btn">
+      <el-button :icon="Star" type="danger" round @click="$router.push('/donate')">
+        赛博要饭
+      </el-button>
+    </div>
     <el-menu mode="horizontal" router>
       <el-menu-item index="/">证书管理</el-menu-item>
       <el-menu-item index="/cert">创建证书</el-menu-item>
       <el-menu-item index="/hosts">hosts编辑</el-menu-item>
-      <el-menu-item index="/donate">赛博要饭</el-menu-item>
     </el-menu>
 
     <div class="content">
@@ -28,5 +32,13 @@
   flex: 1;
   padding: 20px;
   overflow-y: auto;
+}
+
+.donate-btn {
+  position: fixed;
+  top: 0;
+  right: 0;
+  margin: 10px;
+  z-index: 1;
 }
 </style>
